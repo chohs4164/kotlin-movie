4651228c40c011f0045f3fd9eccb1f2b9f0507e5
chohs4164 <josh100as@gmail.com>
2026-04-17 03:15:44 +0900

    feat: 스프링 부트 실행 환경 구성

--- src/main/kotlin/movie/Main.kt
+++ src/main/kotlin/movie/Main.kt
@@ -1,32 +1,11 @@
 package movie
 
-import movie.controller.MovieController
-import movie.infrastructure.db.JdbcReservationRepository
-import movie.infrastructure.db.JdbcScreeningRepository
-import movie.infrastructure.db.SchemaInitializer
-import java.sql.DriverManager
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
-fun main() {
-    DriverManager.getConnection(
-        "jdbc:h2:file:./build/db/movie;AUTO_SERVER = TRUE",
-        "sa",
-        "",
-    ).use { connection ->
-        val movieFixtures = MovieFixtures()
-        val screeningRepository = JdbcScreeningRepository(connection)
-        val reservationRepository = JdbcReservationRepository(connection)
+@SpringBootApplication
+class Application
 
-        SchemaInitializer().initialize(connection)
-
-        // 시드 중복 방지용
-        if (!screeningRepository.hasScreenings()) {
-            screeningRepository.saveAll(movieFixtures.screeningMovieList)
-        }
-
-        MovieController(
-            screeningRepository = screeningRepository,
-            reservationRepository = reservationRepository,
-            movieFixtures = movieFixtures
-        ).run()
-    }
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
